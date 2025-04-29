b8f6b84b571a8c8abb6246ae08d5977b21f1d530
qanusha436 <amoghajoshi2005@gmail.com>
2025-04-29 10:32:14 +0530

    saved

---
+++ arth.java
@@ -0,0 +1,12 @@
+public class arth {
+    public static void main(String[] args){
+        int a=5, b=3;
+        int sum=a+b;
+        int sub=a-b;
+        int mult=a*b;
+        System.out.println("addition of two number: "+sum);
+        System.out.println("substration of two number: "+sub);
+        System.out.println("multipletation of two number: "+mult);
+    }
+    
+}
